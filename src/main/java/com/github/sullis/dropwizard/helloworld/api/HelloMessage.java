bbda0af0ca2309ac5eabd7efa84c595ecba52215
Max Griffiths <maxamg@gmail.com>
2017-03-12 00:37:14 -0600

    Add host info to helloworld service

--- src/main/java/com/github/sullis/dropwizard/helloworld/api/HelloMessage.java
+++ src/main/java/com/github/sullis/dropwizard/helloworld/api/HelloMessage.java
@@ -1,10 +1,19 @@
 package com.github.sullis.dropwizard.helloworld.api;
 
 import java.util.Date;
+import java.net.InetAddress;
 
 public class HelloMessage {
 	private String message = "Hello";
 	private Date timestamp = new Date();
+	private String hostname = getMyHostName();
+
+	public String getHostname() {
+		return hostname;
+	}
+	public void setHostname(InetAddress hostname) {
+		this.hostname = hostname.toString();
+	}
 
 	public Date getTimestamp() {
 		return timestamp;
@@ -20,5 +29,21 @@ public class HelloMessage {
 	public void setMessage(String msg) {
 		message = msg;
 	}
+
+    public String getMyHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName().toString();
+        } catch (Exception e) { return "error"; }
+    }
+
+    private static String getIpAsString(InetAddress address) {
+        byte[] ipAddress = address.getAddress();
+        StringBuffer str = new StringBuffer();
+        for(int i=0; i<ipAddress.length; i++) {
+            if(i > 0) str.append('.');
+            str.append(ipAddress[i] & 0xFF);                
+        }
+        return str.toString();
+    }
 
 }
